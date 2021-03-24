edb03a2f60824008a794182799741bce488af367
Edward Deam <edwardcrdeam@gmail.com>
2021-03-24 15:19:36 +1100

    Task Class setup

---
+++ task.rb
@@ -0,0 +1,26 @@
+#Superclass
+class Task
+  @id = 0
+  @object_count = 0
+
+  def self.next_id 
+    @id += 1
+    return @id
+  end
+
+  def self.object_count=(value)
+    @object_count = value
+  end
+  def self.object_count
+    @object_count
+  end
+
+  attr_accessor :id, :name, :description
+
+  def initialize(name, description = '')
+    Task.object_count=(1)
+    @id = Task.next_id
+    @name = name
+    @description = description
+  end
+end
